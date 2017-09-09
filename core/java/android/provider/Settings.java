ecb29c166b94f8ea6e7364c553bdb3698a4c9855
xyyx <xyyx@mail.ru>
2017-09-09 10:12:48 +0300

    base: Lockscreen Charging info  (3/3)
        based on: [2/2] frameworks: show charging current & voltage on lockscreen by
        yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
        yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
        4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
        AICPfy:
        - default set to false
        - Current formatting improvement for devices reporting directly in Amps
    
        Return-logic of computePowerIndication has changed in Q, so adapted
        it to the new logic.
    
        Ported to 11 & 12 by eyosen: adapt to the move to SettingsLib
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6260,6 +6260,12 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6411,6 +6417,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL);
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL_IGNORE_DND);
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL_RATE);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO);
         }
 
         /**
