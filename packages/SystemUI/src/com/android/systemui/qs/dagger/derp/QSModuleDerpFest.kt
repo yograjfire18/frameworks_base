b5280beb99e4222c2f099ace2d4758af6c149ddd
romanbb <romanbirg@gmail.com>
2018-09-20 23:22:51 +0530

    SystemUI: QS: Add Compass tile
    - Port to 11, 12 - idoybh (YAAP)
    - Port to 10 - Stallix
    - Port to Pie - SuperDroidBond
    - Port to Oreo - LorDClockAN
    
    Change-Id: I86c784644e83cba2a4eacc0b9adb9abf1a334a2b
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    
    -----
    
    SystemUI: CompassTile: Set default state inactive
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I259c251ff4b9740a09c4d31d770f82549bff1eae
    
    -----
    
    SystemUI: CompassTile: Do not use slashed icon
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: I762f8e486cb9bc908d0307a66de2d1d667406c1c
    
    -----
    
    SystemUI: CompassTile: Disable long click
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I61f3b5f7168a5dcce21d0fa862c29237347a4643
    Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/derp/QSModuleDerpFest.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/derp/QSModuleDerpFest.kt
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.RebootTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.WifiTile
+import com.android.systemui.qs.tiles.CompassTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -122,4 +123,10 @@ interface QSModuleDerpFest {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
+
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 }
