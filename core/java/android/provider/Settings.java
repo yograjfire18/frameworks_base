80797478240efea9ed3f83e359b85fe81904b4ba
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: programminghoch10 <hoch10@kabelbw.de>
    Co-authored-by: Sam Mortimer <sam@mortimer.me.uk>
    Change-Id: If22c97eb1ce4cfb13396d182d90786f4b7acaee7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12720,6 +12720,14 @@ public final class Settings {
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
         /**
+         * Whether volume panel should appear on the left (or right).
+         * 0 = false (on the right)
+         * 1 = true (on the left)
+         * @hide
+         */
+        public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
