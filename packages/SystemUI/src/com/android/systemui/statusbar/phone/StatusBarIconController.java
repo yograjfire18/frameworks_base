eb1bd3ae031914ce71eaa389e8f45b95fff06998
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2] [marshmello61: adapt for derpfest]
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -394,7 +394,7 @@ public interface StatusBarIconController {
         protected boolean mDemoable = true;
         private boolean mIsInDemoMode;
         protected DemoStatusIcons mDemoStatusIcons;
-
+            
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
         private boolean mShowWifiStandard;
@@ -403,6 +403,11 @@ public interface StatusBarIconController {
 
         private final boolean mNewIconStyle;
         private final boolean mShowNotificationCount;
+
+        private boolean mOldStyleType;
+
+        private static final String USE_OLD_MOBILETYPE =
+            "system:" + Settings.System.USE_OLD_MOBILETYPE;
 
         public IconManager(
                 ViewGroup group,
@@ -576,6 +581,7 @@ public interface StatusBarIconController {
             StatusBarMobileView mobileView = onCreateStatusBarMobileView(state.subId, slot);
             mobileView.applyMobileState(state);
             mGroup.addView(mobileView, index, onCreateLayoutParams());
+            Dependency.get(TunerService.class).addTunable(this, USE_OLD_MOBILETYPE);
 
             if (mIsInDemoMode) {
                 Context mobileContext = mMobileContextProvider
@@ -837,6 +843,11 @@ public interface StatusBarIconController {
                         TunerService.parseIntegerSwitch(newValue, false);
                     updateShowWifiStandard();
                     break;
+                case USE_OLD_MOBILETYPE:
+                    mOldStyleType =
+                        TunerService.parseIntegerSwitch(newValue, true);
+                    updateOldStyleMobileDataIcons();
+                    break;
                 default:
                     break;
             }
@@ -848,6 +859,15 @@ public interface StatusBarIconController {
                 if (child instanceof StatusBarWifiView) {
                     ((StatusBarWifiView) child).updateWifiState(mShowWifiStandard);
                     return;
+                }
+            }
+        }
+
+        private void updateOldStyleMobileDataIcons() {
+            for (int i = 0; i < mGroup.getChildCount(); i++) {
+                View child = mGroup.getChildAt(i);
+                if (child instanceof StatusBarMobileView) {
+                    ((StatusBarMobileView) child).updateDisplayType(mOldStyleType);
                 }
             }
         }
