298130e52c0db4265bc6517a545e6de990eb45b2
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    idoybh:
    * Add the settings value to backup array yet again
    * update .gitignore (unrelated)
    * Adapted to Sv2
    
    Squashed with:
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Sun Feb 12 15:49:51 2023 +0200
    
        base: Allow controlling statusbar battery % display when charging [1/2]
    
        This actually kills 2 birds with 1 stone.
        User can control whether % jumps outside when inside (was default)
        and can show only while charging when % is disabled entirely
    
        Also improve some other code laying around
        simplify logic, remove useless checks and better document
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Thu Feb 17 05:03:48 2022 +0200
    
        base: SystemUI: Fix circle battery QS tinting
    
        Battery background arc was tinted too close to differ
        Replace textColorSecodary with textColorHint
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sat May 8 13:55:21 2021 +0000
    
        base: Remove aosp's 'show battery percentage' setting
    
        This setting does nothing in yaap so let's not show it.
        YAAP has it's own implementation for this setting in YASP.
    
        Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Wed Feb 14 00:24:18 2018 +0100
    
        BatteryMeterView: Show a bolt ⚡when charging
    
        idoybh:
        * Only when using the text only style
        * Improve some code visually: make it easier to read, use one less var, correctly indent and don't call an inner function var with a name format used for globals
        * Keep the added perf improvement in A13, adapt to it
    
        Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Sun Nov 29 02:08:20 2020 +0200
    
        base: Allow choosing whether to show % inside battery icon [1/2]
    
        Change-Id: I5987cf9fee6ea59532e8d7f2f484be9b1dfa9ee3
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Sat Nov 28 19:36:51 2020 +0200
    
        BatteryMeterView: Remove the icon view if we are not using it
    
        Having the view while not really using it (vis is GONE) is expansive.
    
        Also improve some code while at it:
        * Properly support multi users
        * Use the settings observer instead of tunables
        * Fix AOSP derp for estimates, where it would not be registered for change after a user switch
        * Properly document the settings string
        * Misc improvements
    
        Change-Id: If35b003176234c6a2aef0a4b09ab166d87f6052a
    
    Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -76,6 +76,7 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_SOUND,
         Settings.System.ACCELEROMETER_ROTATION,
         Settings.System.SHOW_BATTERY_PERCENT,
+        Settings.System.SHOW_BATTERY_PERCENT_CHARGING,
         Settings.System.ALARM_VIBRATION_INTENSITY,
         Settings.System.MEDIA_VIBRATION_INTENSITY,
         Settings.System.NOTIFICATION_VIBRATION_INTENSITY,
@@ -88,5 +89,7 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.QS_SHOW_BATTERY_ESTIMATE,
+        Settings.System.STATUS_BAR_BATTERY_STYLE,
+        Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
     };
 }
