cdc513c35c6b260ecc0c3724d11a0e48ec78a760
Michele Bono <bono.michele94@gmail.com>
2020-05-17 15:16:17 +0200

    Ambient Pulse Lights: Add light width [1/2]
    Change-Id: I1fbbf38c6e983230131b58f0caedf2559db5b8df

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6453,6 +6453,12 @@ public final class Settings {
         public static final String AMBIENT_LIGHT_LAYOUT = "ambient_light_layout";
 
         /**
+         * Width of Ambient edge light in px
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_WIDTH = "pulse_ambient_light_width";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
