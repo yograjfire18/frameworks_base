80f505f11b6cf37ee32be736145c815f0c901dad
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-05 22:21:04 +0300

    base: Allow choosing a custom vibration pattern [1/3]
    Change-Id: I0d00f94b185a1032deb3e8413720d47a411cd6f7

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -120,7 +120,8 @@ public class SystemSettings {
                 Settings.System.QS_SHOW_BATTERY_ESTIMATE,
                 Settings.System.STATUS_BAR_BATTERY_STYLE,
                 Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
-                Settings.System.RINGTONE_VIBRATION_PATTERN
+                Settings.System.RINGTONE_VIBRATION_PATTERN,
+                Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
