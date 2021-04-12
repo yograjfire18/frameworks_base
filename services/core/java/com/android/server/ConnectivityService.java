52633b06df1523c7c2763bc8f614a1acd89dfea4
Oliver Scott <olivercscott@gmail.com>
2021-04-12 14:52:44 -0400

    Make isUidIsolated public
    Change-Id: I5a646690ff8de4ccf0802c6159e20520efa40185

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -2062,7 +2062,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
         reattachNetworkRequestsForUid(uid);
     }
 
-    private boolean isUidIsolated(int uid) {
+    @Override
+    public boolean isUidIsolated(int uid) {
         synchronized (mIsolatedUids) {
             return mIsolatedUids.get(uid);
         }
