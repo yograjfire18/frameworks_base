ba9c984179e9d3ffd3dc464b57101238e68ac566
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2021-10-16 10:02:58 +0000

    SystemUI: Add Sound tile to Quick Settings
    [ghostrider-reborn: misc improvements and cleanup]
    
    Squashed commit of the following:
    
    commit 69f86d9a38b5a942e3f9c914895ee45c0079e8e9
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Apr 13 03:32:51 2018 +0200
    
        base: SystemUI: stop crashing qs tiles during boot
    
        this triggers my ocd
        continues a579c2430f1a0bf088023b4283dcec37776c070d
    
        Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
        Signed-off-by: xyyx <xyyx@mail.ru>
    
    commit f419619c033fa99a809f53aa82dbdd7d6732177c
    Author: xyyx <xyyx@mail.ru>
    Date:   Thu Sep 28 11:55:36 2017 +0800
    
        SoundTile: Change ZEN_MODE_NO_INTERRUPTIONS to ZEN_MODE_ALARMS
    
        Change-Id: I5edaaa4d551630049b29e09d3cecfb5ffd503e99
    
    commit 916645271f57c47d60bb0a04c4e51776b51a18f1
    Author: Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
    Date:   Sun Sep 18 22:33:08 2016 +0200
    
        Add Sound tile to Quick Settings
    
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
    
    Change-Id: I178bfa69ff181f0f65f94ffae1444aaea5f21a80
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/derp/QSModuleDerpFest.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/derp/QSModuleDerpFest.kt
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.CompassTile
+import com.android.systemui.qs.tiles.SoundTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -129,4 +130,10 @@ interface QSModuleDerpFest {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject SoundTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SoundTile.TILE_SPEC)
+    fun bindSoundTile(soundTile: SoundTile): QSTileImpl<*>
 }
