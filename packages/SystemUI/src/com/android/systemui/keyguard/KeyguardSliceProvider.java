ShapeShiftOS/android_frameworks_base

keyguard: Fix null text on 12 clock after dismissing media

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -537,7 +537,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         }
 
         // Set new track info from playing media notification
-        if (isShapeShiftTwelveClockSelected) {
+        if (isShapeShiftTwelveClockSelected && title != null) {
             StringBuffer evenSB = new StringBuffer(" ");
             evenSB.append(title);
             mMediaTitle = evenSB;

yograjfire18/frameworks_base

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -649,7 +649,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         }
 
         // Set new track info from playing media notification
-        if (isTwelveClockSelected) {
+        if (isTwelveClockSelected && title != null) {
             StringBuffer evenSB = new StringBuffer(" ");
             evenSB.append(title);
             mMediaTitle = evenSB;