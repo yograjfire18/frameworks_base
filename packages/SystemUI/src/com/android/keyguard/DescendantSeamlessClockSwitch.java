anuragbhaumik/frameworks_base-2

DescendantSeamlessClockSwitch: Update and order the clocks
Change-Id: I09b0c234201ed55a54be1558dbdccee16d90999e
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/keyguard/DescendantSeamlessClockSwitch.java
@@ -25,28 +25,31 @@ public class DescendantSeamlessClockSwitch {
         "com.android.keyguard.clock.DefaultClockController",
         "com.android.keyguard.clock.DefaultBoldClockController",
         "com.android.keyguard.clock.AnalogClockController",
+        "com.android.keyguard.clock.AndroidSClockController",
+        "com.android.keyguard.clock.AndroidSDP3ClockController",
+        "com.android.keyguard.clock.Android12ClockController",
         "com.android.keyguard.clock.BubbleClockController",
+        "com.android.keyguard.clock.ClockertinoClockController",
+        "com.android.keyguard.clock.DividedLinesClockController",
+        "com.android.keyguard.clock.FluidClockController",
+        "com.android.keyguard.clock.Fluidv2ClockController",
+        "com.android.keyguard.clock.GradientFireClockController",
+        "com.android.keyguard.clock.IDEClockController",
+        "com.android.keyguard.clock.IDEJavaClockController",
+        "com.android.keyguard.clock.MNMLBoxClockController",
+        "com.android.keyguard.clock.MNMLMinimalClockController",
+        "com.android.keyguard.clock.OctaviDigitalClockController",
+        "com.android.keyguard.clock.OOSClockController",
+        "com.android.keyguard.clock.OronosClockController",
+        "com.android.keyguard.clock.OronosReshapedClockController",
         "com.android.keyguard.clock.TypeClockController",
         "com.android.keyguard.clock.TypeClockCenteredController",
         "com.android.keyguard.clock.TypeAltClockController",
         "com.android.keyguard.clock.SamsungClockController",
         "com.android.keyguard.clock.SamsungBoldClockController",
         "com.android.keyguard.clock.SamsungHighlightClockController",
-        "com.android.keyguard.clock.ClockertinoClockController",
-        "com.android.keyguard.clock.DividedLinesClockController",
-        "com.android.keyguard.clock.FluidClockController",
-        "com.android.keyguard.clock.GradientFireClockController",
-        "com.android.keyguard.clock.IDEClockController",
-        "com.android.keyguard.clock.IDEJavaClockController",
-        "com.android.keyguard.clock.MNMLBoxClockController",
-        "com.android.keyguard.clock.MNMLMinimalClockController",
         "com.android.keyguard.clock.SfunyClockController",
-        "com.android.keyguard.clock.OOSClockController",
-        "com.android.keyguard.clock.StickerTagClockController",
-        "com.android.keyguard.clock.OronosClockController",
-        "com.android.keyguard.clock.OronosReshapedClockController",
-        "com.android.keyguard.clock.OctaviDigitalClockController",
-        "com.android.keyguard.clock.Fluidv2ClockController"
+        "com.android.keyguard.clock.StickerTagClockController"
     };
 
     public static int getCurrentPosition(String currentClockFace) {

yograjfire18/frameworks_base

packages/SystemUI/src/com/android/keyguard/DescendantSeamlessClockSwitch.java
@@ -25,28 +25,29 @@ public class DescendantSeamlessClockSwitch {
         "com.android.keyguard.clock.DefaultClockController",
         "com.android.keyguard.clock.DefaultBoldClockController",
         "com.android.keyguard.clock.AnalogClockController",
+        "com.android.keyguard.clock.AndroidTwelveClockController",
         "com.android.keyguard.clock.BubbleClockController",
-        "com.android.keyguard.clock.TypeClockController",
-        "com.android.keyguard.clock.TypeClockCenteredController",
-        "com.android.keyguard.clock.TypeAltClockController",
-        "com.android.keyguard.clock.SamsungClockController",
-        "com.android.keyguard.clock.SamsungBoldClockController",
-        "com.android.keyguard.clock.SamsungHighlightClockController",
         "com.android.keyguard.clock.ClockertinoClockController",
         "com.android.keyguard.clock.DividedLinesClockController",
         "com.android.keyguard.clock.FluidClockController",
+        "com.android.keyguard.clock.Fluidv2ClockController",
         "com.android.keyguard.clock.GradientFireClockController",
         "com.android.keyguard.clock.IDEClockController",
         "com.android.keyguard.clock.IDEJavaClockController",
         "com.android.keyguard.clock.MNMLBoxClockController",
         "com.android.keyguard.clock.MNMLMinimalClockController",
-        "com.android.keyguard.clock.SfunyClockController",
+        "com.android.keyguard.clock.OctaviDigitalClockController",
         "com.android.keyguard.clock.OOSClockController",
-        "com.android.keyguard.clock.StickerTagClockController",
         "com.android.keyguard.clock.OronosClockController",
         "com.android.keyguard.clock.OronosReshapedClockController",
-        "com.android.keyguard.clock.OctaviDigitalClockController",
-        "com.android.keyguard.clock.Fluidv2ClockController"
+        "com.android.keyguard.clock.SamsungClockController",
+        "com.android.keyguard.clock.SamsungBoldClockController",
+        "com.android.keyguard.clock.SamsungHighlightClockController",
+        "com.android.keyguard.clock.SfunyClockController",
+        "com.android.keyguard.clock.StickerTagClockController",
+        "com.android.keyguard.clock.TypeClockController",
+        "com.android.keyguard.clock.TypeClockCenteredController",
+        "com.android.keyguard.clock.TypeAltClockController"
     };
 
     public static int getCurrentPosition(String currentClockFace) {