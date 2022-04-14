3ecffd4c17f264a2845216afbba5d14a5c1fe926
George Zacharia <george.zcharia@gmail.com>
2022-04-14 19:37:05 +0530

    SystemUI: DerpSpaceTile: Don't support long press if Updater is not bundled
    Well, will miss "that" toast, but eliminate any crashes for now
    
    Change-Id: Id0d69e055a059e0d5da2e2f9357635cf9903ae99

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DerpSpaceTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DerpSpaceTile.java
@@ -82,7 +82,9 @@ public class DerpSpaceTile extends QSTileImpl<State> {
 
     @Override
     public State newTileState() {
-        return new State();
+        State state = new State();
+        state.handlesLongClick = isOTABundled() ? true : false;
+        return state;
     }
 
     @Override
