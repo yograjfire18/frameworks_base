6757b0b1b51020b7747d4a98661be67aa9b18a88
Arian <arian.kulmer@web.de>
2022-08-20 23:38:25 +0200

    udfps: Restore illumination dot for global hbm
    UdfpsSurfaceView.java is imported from android-12.1.0_r22
    
    idoybh (YAAP): Adapted, partially pick only what we need (we merged A15),
    Other minor code improvements
    
    Change-Id: I4a4e85a7437a9a444a4f952fd62e4fe12f56ce5a

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsSurfaceView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsSurfaceView.java
@@ -98,7 +98,7 @@ public class UdfpsSurfaceView extends SurfaceView implements SurfaceHolder.Callb
         mHasValidSurface = false;
     }
 
-    void setGhbmIlluminationListener(@Nullable GhbmIlluminationListener listener) {
+    public void setGhbmIlluminationListener(@Nullable GhbmIlluminationListener listener) {
         mGhbmIlluminationListener = listener;
     }
 
@@ -107,7 +107,7 @@ public class UdfpsSurfaceView extends SurfaceView implements SurfaceHolder.Callb
      * {@link UdfpsView} will hide this view, which would destroy the surface and remove the
      * illumination dot.
      */
-    void startGhbmIllumination(@Nullable Runnable onDisplayConfigured) {
+    public void startGhbmIllumination(@Nullable Runnable onDisplayConfigured) {
         if (mGhbmIlluminationListener == null) {
             Log.e(TAG, "startIllumination | mGhbmIlluminationListener is null");
             return;
@@ -133,7 +133,7 @@ public class UdfpsSurfaceView extends SurfaceView implements SurfaceHolder.Callb
     /**
      * Immediately draws the illumination dot on this SurfaceView's surface.
      */
-    void drawIlluminationDot(@NonNull RectF sensorRect) {
+    public void drawIlluminationDot(@NonNull RectF sensorRect) {
         if (!mHasValidSurface) {
             Log.e(TAG, "drawIlluminationDot | the surface is destroyed or was never created.");
             return;
