c3bcb8d81fb63cc318d58bd6c693d6a57fb7771e
LibXZR <i@xzr.moe>
2022-09-14 12:07:42 +0800

    SystemUI: Fix status bar lyric overlapping with the center clock
    Change-Id: I8a058e1239d504f92ed811e528cff2077444a8d2
    Signed-off-by: LibXZR <i@xzr.moe>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -858,10 +858,12 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private class LyricController extends LyricViewController {
         private View mStartSide;
+        private View mCenteredArea;
 
         public LyricController(Context context, View statusBar) {
             super(context, statusBar);
             mStartSide = statusBar.findViewById(R.id.status_bar_start_side_except_heads_up);
+            mCenteredArea = statusBar.findViewById(R.id.centered_area);
         }
 
         public void showLyricView(boolean animate) {
@@ -869,6 +871,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             boolean hasOngoingCall = (mDisabled1 & DISABLE_ONGOING_CALL_CHIP) == 0;
             if (!disableNotifications && !hasOngoingCall && isLyricStarted()) {
                 animateHide(mStartSide, animate);
+                animateHide(mCenteredArea, animate);
                 animateShow(getView(), animate);
             }
         }
@@ -876,6 +879,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         public void hideLyricView(boolean animate) {
             animateHide(getView(), animate);
             animateShow(mStartSide, animate);
+            animateShow(mCenteredArea, animate);
         }
     }
 }
