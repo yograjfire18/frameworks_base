9c2aba630f39877889112482f76550771187ac6c
Vishal Cj <vishalcj@aospa.co>
2022-09-23 05:32:54 +0700

    udfps: Forward port Boost framework hook.
    * In my testing on OnePlus9 Pro, this had a drastic effect.
      Speeds went from 0.87s to 0.38s (average from 10 trials)
    
    * 2 second headroom is enough as the PerfLock is triggered
      showUdfpsOverlay and released onFingerUp.
    
    Change-Id: Icb624961f27c61ca5d55caf1e016f5fc2f19c57c

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -52,6 +52,7 @@ import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.util.BoostFramework;
 import android.util.Log;
 import android.util.RotationUtils;
 import android.view.LayoutInflater;
@@ -209,6 +210,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private int mAutoModeState;
 
     private UdfpsAnimation mUdfpsAnimation;
+
+    // Boostframework for UDFPS
+    private BoostFramework mPerf = null;
+    private boolean mIsPerfLockAcquired = false;
+    private static final int BOOST_DURATION_TIMEOUT = 2000;
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -838,6 +844,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mTriggerOnFingerDownForActionDown = mContext.getResources().getBoolean(
                 R.bool.config_udfpsTriggerOnFingerDownForActionDown);
 
+        mPerf = new BoostFramework();
+
         mAmbientDisplayConfiguration = new AmbientDisplayConfiguration(mContext);
         updateScreenOffFodState();
         mSecureSettings.registerContentObserver(Settings.Secure.SCREEN_OFF_UDFPS_ENABLED,
@@ -929,6 +937,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mOrientationListener.enable();
         } else {
             Log.v(TAG, "showUdfpsOverlay | the overlay is already showing");
+        }
+
+        if (mPerf != null && !mIsPerfLockAcquired) {
+            mPerf.perfHint(BoostFramework.VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                    null,
+                    BOOST_DURATION_TIMEOUT);
+            mIsPerfLockAcquired = true;
         }
     }
 
@@ -1219,6 +1234,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             unconfigureDisplay(view);
         }
         cancelAodSendFingerUpAction();
+
+       if (mPerf != null && mIsPerfLockAcquired) {
+            mPerf.perfLockRelease();
+            mIsPerfLockAcquired = false;
+        }
     }
 
     /**
