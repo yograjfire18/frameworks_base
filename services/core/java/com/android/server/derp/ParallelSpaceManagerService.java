0d67725646cf95a5ac92f0d96707841936aaad54
LibXZR <i@xzr.moe>
2022-09-25 16:19:48 +0800

    ParallelSpaceManagerService: Do not install setupwizard too
    Not needed and may confuse users when something goes wrong.
    
    Change-Id: Iced7b7ae6c23a75484994f6154b7887abb0c5653
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- services/core/java/com/android/server/derp/ParallelSpaceManagerService.java
+++ services/core/java/com/android/server/derp/ParallelSpaceManagerService.java
@@ -103,7 +103,9 @@ public final class ParallelSpaceManagerService extends SystemService {
 
     private static final List<String> SPACE_BLACKLIST_PACKAGES = Arrays.asList(
         // To avoid third party apps starting it accidentally.
-        "com.android.launcher3"
+        "com.android.launcher3",
+        "com.google.android.pixel.setupwizard",
+        "com.google.android.setupwizard"
     );
 
     /**
