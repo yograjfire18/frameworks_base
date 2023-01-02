31268bb73a2a90be956667e02096539f4345a9c6
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-03 07:03:48 +0800

    SystemUI: Move bluetooth dialog creation to long press
    This should make bluetooth tile behave as intended and allows users
    to have better UX in picking a BT device.
    
    Change-Id: I08009911dbbaf337f2767946b3092db7495671e9
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [ghostrider-reborn: disable forceExpandIcon]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -88,9 +88,7 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
 
     @Override
     public BooleanState newTileState() {
-        BooleanState s = new BooleanState();
-        s.forceExpandIcon = true;
-        return s;
+        return new BooleanState();
     }
 
     @Override
@@ -98,12 +96,22 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
         if (checkKeyguard(view, keyguardShowing)) {
             return;
         }
+
+        // Secondary clicks are header clicks, just toggle.
+        final boolean isEnabled = mState.value;
+        // Immediately enter transient enabling state when turning bluetooth on.
+        refreshState(isEnabled ? null : ARG_SHOW_TRANSIENT_ENABLING);
+        mController.setBluetoothEnabled(!isEnabled);
+    }
+
+    @Override
+    protected void handleLongClick(@Nullable View view) {
         mHandler.post(() -> mBluetoothDialogFactory.create(true, view));
     }
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+        return null;
     }
 
     @Override
