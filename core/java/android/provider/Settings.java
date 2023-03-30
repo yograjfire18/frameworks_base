d876d8c7d8d9830233cf235a3c9dd7c83438d629
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-30 22:33:05 +0800

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: Hikari-no-Tenshi/crdroid_android_frameworks_base@354d981
    
    mnri: rewrite and refactored for QPR3
    
    Squashed with:
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Sun Jun 11 15:14:34 2023 +0800
    
        SystemUI: Convert MIUI8/9 QS Header resources to webp format
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Sat Jun 3 17:43:07 2023 +0800
    
        SystemUI: Adds transparent gradient to the MIUI8/9 QS Header resources
    
        Hmmm...
    
    Author: Pzqqt <821026875@qq.com>
    Date:   Sat Jun 3 12:22:33 2023 +0800
    
        SystemUI: Import MIUI 8/9 QS Header resources
    
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Change-Id: I9bd0a7a9f91096c8462b2b08f87a9a39f24cdff3
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6698,6 +6698,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+         * QS Header
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE = "qs_header_image";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
